3096b2e4dbc36a4dca4cabbf05ef14ad41638a6d
tiger2221504 <taiga_606@yahoo.co.jp>
2023-11-22 12:20:42 +0900

    Insertクラスを作成した

--- prmn2023b/src/main/java/jp/ac/chitose/db_prac/PreExamDAO.java
+++ prmn2023b/src/main/java/jp/ac/chitose/db_prac/PreExamDAO.java
@@ -11,7 +11,7 @@ public class PreExamDAO {
 
     public List<PreExam> selectPreExams(int lessThan) throws SQLException{
         List<PreExam> returning = new ArrayList<>();
-        String SQL = "select * from 学生情報 where 得点 < lessThan";
+        String SQL = "select * from 学生情報 where 得点 < ?";
 
         try (Connection conn = DriverManager.getConnection(URL,USER_NAME,USER_PASS);
              PreparedStatement stmt = conn.prepareStatement(SQL)){
